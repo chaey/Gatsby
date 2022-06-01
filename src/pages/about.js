f77b049c8b5d4bc1f3ccecbe798565b96535b41d
foxrhee <foxrhee@gmail.com>
2022-06-01 16:05:11 +0900

    adding About page to site

---
+++ src/pages/about.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+
+export default ({data}) => (
+  <React.Fragment>
+    <h2>{data.site.siteMetadata.title}</h2>
+    <div>{data.site.siteMetadata.description}</div>
+    <div>{data.site.siteMetadata.siteUrl}</div>
+    <div>{data.site.siteMetadata.author}</div>
+    <Link to="/">Back to home</Link>
+  </React.Fragment>
+)
+
+export const query = graphql `
+  query MyQuery {
+    site {
+      siteMetadata {
+        author
+        description
+        siteUrl
+        title
+      }
+    }
+  }
+`
